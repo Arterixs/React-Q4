b08be87f348d3ce33701e7c35bf125dc584d7d43
Arterixs <artemontugg@gmail.com>
2023-10-25 22:19:41 +0300

    feat: add eslint in project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,42 @@
+const OFF = 0;
+const WARN = 1;
+const ERROR = 2;
+
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['react-refresh', '@typescript-eslint'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    'react-refresh/only-export-components': [WARN, { allowConstantExport: true }],
+    'react/jsx-no-constructed-context-values': OFF,
+    'react/jsx-props-no-spreading': [
+      ERROR,
+      {
+        html: 'ignore',
+      },
     ],
+    'import/prefer-default-export': OFF,
+    'react/function-component-definition': [
+      'warn',
+      {
+        namedComponents: 'arrow-function',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': WARN,
   },
 }
